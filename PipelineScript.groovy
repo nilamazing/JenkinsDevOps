82defd7b42658f0b4bdbe82939d8a64dae9b0b6a
nilamazing <duttnilanjan@gmail.com>
2020-06-01 21:21:57 +0100

    Update Pipeline Code and Instructions

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -21,9 +21,22 @@ node{
           sh label: 'Check docker version', script: 'docker -v'
     }
     stage("Build Docker Image"){
-          sh label: 'Build Docker Image', script: 'docker build -t testnodeproj:1.0.0 -f Dockerfile .'
+          sh label: 'Build Docker Image', script: 'docker build -t nilamazing/testnodeproj:1.0.0 -f Dockerfile .'
     }
     stage("List Docker Images"){
           sh label: 'List Docker Images', script: 'docker images | grep testnodeproj'
+    }
+    stage("Docker Hub Login and Push Image"){
+        withCredentials([string(credentialsId: 'docker-hub-pwd', variable: 'dockerHubPwd')]) {
+             sh label: 'Docker Hub Login', script: "docker login -u nilamazing -p ${dockerHubPwd}"
+             sh label: 'Docker Push Image', script: "docker push nilamazing/testnodeproj:1.0.0"
+        }
+    }
+    stage("Run Docker Container in Target Host"){
+        def dockerCmd = 'docker run -d --name TestNodeServer -p 8081:8081 nilamazing/testnodeproj:1.0.0'
+        sshagent(['ssh-pvt-key-mylinux']) {
+            sh "ssh -o StrictHostKeyChecking=no demo@192.168.0.93 ${dockerCmd}"
+            
+        }
     }
 }
